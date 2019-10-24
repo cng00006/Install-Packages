84c1a8295b195c8d3781cc26c0cb62fc0f8093e3
cng00006 <55862984+cng00006@users.noreply.github.com>
2019-10-23 20:32:38 -0700

    Update installed_check.sh

--- installed_check.sh
+++ installed_check.sh
@@ -3,8 +3,7 @@
 rpm -q $1 &> /dev/null
 
 if [ $? -eq 0 ]; then
-    echo "Package installed"
-    exit 0;        
+    echo "Package installed"        
 else
     while true; do
         read -p "Do you want to install this package?" yn
@@ -15,3 +14,14 @@ else
         esac
      done
 fi
+varnames=$(systemctl status $1 | grep Active | awk ‘{print$2}’)
+varnames2=“inactive”
+
+If [ $varname == $varname2 ]; then
+	while true; do
+        read -p "Service inactive. Do you want to start this service?" yn
+        case $yn in
+            [Yn]* ) systemctl start $1; break;;
+            [Nn]* ) exit;;
+            *)echo "Please answer Y or N"
+fi            
